86406da125d84d2d86f4e7a6aa7ba272dccacf7b
Hugo Hontans <hugohontans@gmail.com>
2018-12-20 15:11:09 +0100

    first commit

---
+++ src/main/java/Com/washmail/washmail/WashmailApplication.java
@@ -0,0 +1,14 @@
+package Com.washmail.washmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashmailApplication.class, args);
+	}
+
+}
+
